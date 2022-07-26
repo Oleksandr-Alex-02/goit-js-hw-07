cf7763f7687a4d6a721f22dd2a7a1c530ea1b433
Oleksandr-Alex-02 <oleksandr.cajkovskij.alex@gmail.com>
2022-07-26 12:28:29 +0300

    task-1-gallerya+

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const divGallery = document.querySelector(".gallery");
+
+function galereaImag(galleryItems) {
+  const array = galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <div class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+          </a>
+        </div>`;
+    })
+    .join("");
+
+  return array;
+}
+
+const arr = galereaImag(galleryItems);
+console.log(arr);
+
+divGallery.insertAdjacentHTML("afterbegin", arr);
